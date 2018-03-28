0d9eec500bcdf438ba7b02917c5aae3ace2e5d26
Vinzenz Feenstra <vfeenstr@redhat.com>
2018-03-28 15:09:15 +0200

    Include the version in the setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import find_packages, setup
+from distutils.util import convert_path
+
+main_ns = {}
+ver_path = convert_path('leapp/__init__.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
 
 setup(
     name='leapp',
+    version=main_ns['VERSION'],
     packages=find_packages(),
     install_requires=['requests', 'six'],
     entry_points='''
