cd32764d0a42ae8c2596d014ff2d867e888f21b0
Vinzenz Feenstra <vfeenstr@redhat.com>
2018-03-28 13:52:02 +0200

    Add six to requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='leapp',
     packages=find_packages(),
-    install_requires=['requests'],
+    install_requires=['requests', 'six'],
     entry_points='''
         [console_scripts]
         snactor=leapp.snactor:main
